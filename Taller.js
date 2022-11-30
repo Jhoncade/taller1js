144ef3e6767ee32bac787b144241f8ce8a3169bd
Jhon Cadena <jhoncadena.23@hotmail.com>
2022-11-30 12:51:29 -0500

    Ultimo comentario

---
+++ Taller.js
@@ -0,0 +1,103 @@
+// let A = 2,B = 5,C=1
+
+// console.log(3 * A - 4 * B / A**2)
+// console.log ( (((B + C) / 2 * A + 10) * 3 * B) - 6)
+
+
+// capital=parseFloat(prompt("Digita tu capital: "))
+
+
+
+// console.log("Lo que ganara segun el capital invertido sera de: ",capital*0.12)
+
+
+// base=parseFloat(prompt("Digita la base: "))
+// altura=parseFloat(prompt("Digita la altura: "))
+
+// console.log("El area del triangulo es: ",base*altura/2)
+
+// let salario=980000, comision=170000
+
+// autos=parseFloat(prompt("Digita el numero de autos vendidos: "))
+// vautos=parseFloat(prompt("Digita el valor de los autos vendidos: "))
+
+// precomi=autos *comision 
+// subcomi=vautos * 0.05
+// precomi2=subcomi*autos
+// comitotal=precomi+precomi2
+// salariototal=salario+comitotal
+
+// console.log("El valor de su salario es:",salariototal)
+
+// nota1=parseFloat(prompt("Digita la primera nota (20%): "))
+// nota2=parseFloat(prompt("Digita la segunda nota (20%): "))
+// nota3=parseFloat(prompt("Digita la tercera nota (30%): "))
+// nota4=parseFloat(prompt("Digita la cuarta nota (30%): "))
+
+
+// cuarenta=((nota1+nota2)*0.4)
+// sesenta=((nota4+nota3)*0.6)
+// total=cuarenta+sesenta/4
+
+
+// console.log("Su nota total es: ",total)
+
+
+// edad=parseInt(prompt("Digita tu edad: "))
+// numpulsaciones = (220 - edad)/10
+
+// console.log("El numero de pulsasiones segun la edad es: ",numpulsaciones)
+
+// sueldo=parseFloat(prompt("Digita el valor de tu sueldo: "))
+// venta1=parseFloat(prompt("Digita el valor de tu primera venta: "))
+// venta2=parseFloat(prompt("Digita el valor de tu segunda venta: "))
+// venta3=parseFloat(prompt("Digita el valor de tu tercera venta: "))
+
+// v1=venta1*0.1
+// v2=venta2*0.1
+// v3=venta3*0.1
+
+// sueldototal=v1+v2+v3+sueldo
+
+// console.log("El sueldo total es: ",sueldototal)
+
+
+
+mate1=parseFloat(prompt("Digita la nota del examen de matematicas: "))
+mate2=parseFloat(prompt("Digita la nota de la primera tarea de matematicas: "))
+mate3=parseFloat(prompt("Digita la nota de la segunda tarea de matematicas:  "))
+mate4=parseFloat(prompt("Digita la nota del la tercera tarea de matematicas:  "))
+
+tareas=(mate2+mate3+mate4)/3
+diezt=tareas*0.1
+novee=mate1*0.9
+totalmate=novee+diezt
+
+console.log("Su promedio en matematicas sera: ",totalmate)
+
+fisi1=parseFloat(prompt("Digita la nota del examen de fisica: "))
+fisi2=parseFloat(prompt("Digita la nota de la primera tarea de fisica: "))
+fisi3=parseFloat(prompt("Digita la nota de la segunda tarea de fisica:  "))
+
+
+tareas=(fisi1+fisi2)/2
+veinf=tareas*0.2
+ochef=fisi1*0.8
+totalfisi=veinf+ochef
+
+
+console.log("Su promedio en fisica sera: ",totalfisi)
+
+quimi1=parseFloat(prompt("Digita la nota del examen de quimica: "))
+quimi2=parseFloat(prompt("Digita la nota de la primera tarea de quimica: "))
+quimi3=parseFloat(prompt("Digita la nota de la segunda tarea de quimica:  "))
+quimi4=parseFloat(prompt("Digita la nota de la tercera tarea de quimica:  "))
+
+
+tareas=(quimi1+quimi2+quimi3)/3
+quinq=tareas*0.15
+ochecinq=fisi1*0.85
+totalquimi=quinq+ochecinq
+
+
+console.log("Su promedio en quimica sera: ",totalquimi)
